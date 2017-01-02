c1f055097c94f86e2aab6f3cc03ff7daf41eab20
AliaaHosny <aliaahosny77@gmail.com>
2017-01-01 19:48:43 -0800

    Adding pages

---
+++ assets/js/scroll.js
@@ -0,0 +1,71 @@
+$(function () {
+
+	// Custom Scrollbar
+	var nice = $("html").niceScroll({
+		cursorwidth: 8,
+		cursorborder: "0px solid #fff",
+		cursorborderradius: '0'
+	});
+
+	$('.main-nav a:not(.dropdown-toggle)').bind('click', function(event) {
+		var $anchor = $(this);
+
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+
+		event.preventDefault();
+	});
+
+	$('[data-toggle="tooltip"]').tooltip();
+    function home_height () {
+		var element = $('.st-home-unit'),
+			elemHeight = element.height(),
+			winHeight = $(window).height()
+			padding = (winHeight - elemHeight - 200) /2;
+
+		if (padding < 1 ) {
+			padding = 0;
+		};
+		element.css('padding', padding+'px 0');
+	}
+	home_height ();
+
+	$(window).resize(function () {
+		home_height ();
+	});
+
+
+	var fadeStart=$(window).height()/3 // 100px scroll or less will equiv to 1 opacity
+    ,fadeUntil=$(window).height() // 200px scroll or more will equiv to 0 opacity
+    ,fading = $('.st-home-unit')
+    ,fading2 = $('.hero-overlayer')
+	;
+
+	$(window).bind('scroll', function(){
+	    var offset = $(document).scrollTop()
+	        ,opacity=0
+	        ,opacity2=1
+	    ;
+	    if( offset<=fadeStart ){
+	        opacity=1;
+	        opacity2=0;
+	    }else if( offset<=fadeUntil ){
+	        opacity=1-offset/fadeUntil;
+	        opacity2=offset/fadeUntil;
+	    }
+	    fading.css({'opacity': opacity});
+
+	    if (offset >= 120) {
+	    	$('.st-navbar').addClass("st-navbar-mini");
+	    } else if (offset <= 119) {
+	    	$('.st-navbar').removeClass("st-navbar-mini");
+	    }
+	});
+});
+
+
+
+
+	
+
